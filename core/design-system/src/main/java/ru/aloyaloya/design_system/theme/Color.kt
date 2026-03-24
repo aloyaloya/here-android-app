982f21d520df466c0b2715cad2b86e6a6f01cb6f
Egor Bugay <egbugaj@gmail.com>
2026-03-24 22:55:56 +0600

    feat: add colors and configure theme

--- core/design-system/src/main/java/ru/aloyaloya/design_system/theme/Color.kt
+++ core/design-system/src/main/java/ru/aloyaloya/design_system/theme/Color.kt
@@ -2,10 +2,34 @@ package ru.aloyaloya.design_system.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val DarkPrimary = Color(0xFF1C0C0F)
+val DarkOnPrimary = Color(0xFFF7E4E8)
+val DarkOnPrimaryFixed = Color(0xFFB79397)
+val DarkPrimaryContainer = Color(0xFF261215)
+val DarkOnPrimaryContainer = Color(0xFF6A363D)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val DarkSecondary = Color(0xFF2C1518)
+val DarkSecondaryContainer = Color(0xFF32191C)
+val DarkOnSecondary = Color(0xFFBE989D)
+val DarkOnSecondaryFixed = Color(0xFF723A41)
+
+val DarkTertiary = Color(0xFF2D1518)
+val DarkOnTertiary = Color(0xFFF7E4E8)
+val DarkOnTertiaryFixed = Color(0xFFBE989D)
+val DarkTertiaryContainer = Color(0xFF35161B)
+
+val LightPrimary = Color(0xFFFFF4F6)
+val LightOnPrimary = Color(0xFF301012)
+val LightOnPrimaryFixed = Color(0xFF91696E)
+val LightPrimaryContainer = Color(0xFFFEE9ED)
+val LightOnPrimaryContainer = Color(0xFFDF969C)
+
+val LightSecondary = Color(0xFFFFFFFF)
+val LightSecondaryContainer = Color(0xFFFCF4F5)
+val LightOnSecondary = Color(0xFF91696E)
+val LightOnSecondaryFixed = Color(0xFFDF939A)
+
+val LightTertiary = Color(0xFFFFFFFF)
+val LightOnTertiary = Color(0xFF301012)
+val LightOnTertiaryFixed = Color(0xFF91696E)
+val LightTertiaryContainer = Color(0xFFFDF6F7)
--- core/design-system/src/main/java/ru/aloyaloya/design_system/theme/Theme.kt
+++ core/design-system/src/main/java/ru/aloyaloya/design_system/theme/Theme.kt
@@ -1,50 +1,49 @@
 package ru.aloyaloya.design_system.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = DarkPrimary,
+    onPrimary = DarkOnPrimary,
+    onPrimaryFixed = DarkOnPrimaryFixed,
+    primaryContainer = DarkPrimaryContainer,
+    onPrimaryContainer = DarkOnPrimaryContainer,
+    secondary = DarkSecondary,
+    secondaryContainer = DarkSecondaryContainer,
+    onSecondary = DarkOnSecondary,
+    onSecondaryFixed = DarkOnSecondaryFixed,
+    tertiary = DarkTertiary,
+    onTertiary = DarkOnTertiary,
+    onTertiaryFixed = DarkOnTertiaryFixed,
+    tertiaryContainer = DarkTertiaryContainer
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = LightPrimary,
+    onPrimary = LightOnPrimary,
+    onPrimaryFixed = LightOnPrimaryFixed,
+    primaryContainer = LightPrimaryContainer,
+    onPrimaryContainer = LightOnPrimaryContainer,
+    secondary = LightSecondary,
+    secondaryContainer = LightSecondaryContainer,
+    onSecondary = LightOnSecondary,
+    onSecondaryFixed = LightOnSecondaryFixed,
+    tertiary = LightTertiary,
+    onTertiary = LightOnTertiary,
+    onTertiaryFixed = LightOnTertiaryFixed,
+    tertiaryContainer = LightTertiaryContainer
 )
 
 @Composable
 fun HereTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
