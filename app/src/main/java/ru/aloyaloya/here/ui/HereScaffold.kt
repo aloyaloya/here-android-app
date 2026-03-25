b27a6fc8da6226b775693812563a3e4205915b1d
Egor Bugay <egbugaj@gmail.com>
2026-03-25 21:47:07 +0600

    feat: update app scaffold background

--- app/src/main/java/ru/aloyaloya/here/ui/HereScaffold.kt
+++ app/src/main/java/ru/aloyaloya/here/ui/HereScaffold.kt
@@ -3,6 +3,7 @@ package ru.aloyaloya.here.ui
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -40,10 +41,9 @@ fun HereScaffold(
     modifier: Modifier = Modifier,
     content: @Composable () -> Unit
 ) {
-    Box(
-        modifier = modifier.fillMaxSize()
-    ) {
+    Box(modifier = modifier.fillMaxSize()) {
         Scaffold(
+            containerColor = MaterialTheme.colorScheme.primary,
             bottomBar = {
                 BottomNavigationBar {
                     destinations.forEach { destination ->
