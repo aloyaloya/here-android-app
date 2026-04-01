c7d948ac61f3454a79e10ba63676354f368af13a
Egor Bugay <egbugaj@gmail.com>
2026-04-01 15:24:29 +0600

    feat: add map screen viewmodel to feature module

--- feature/map/src/main/java/ru/aloyaloya/map/presentation/navigation/MapNavigation.kt
+++ feature/map/src/main/java/ru/aloyaloya/map/presentation/navigation/MapNavigation.kt
@@ -1,10 +1,18 @@
 package ru.aloyaloya.map.presentation.navigation
 
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import kotlinx.serialization.Serializable
+import ru.aloyaloya.map.di.MapComponent
+import ru.aloyaloya.map.presentation.MapScreen
+import ru.aloyaloya.map.presentation.MapViewModel
+import ru.aloyaloya.ui.di.ComponentProvider
 
 @Serializable
 /** Маршрут экрана карты в графе навигации. */
@@ -26,7 +34,21 @@ fun NavController.navigateToMap(navOptions: NavOptions) =
  * размещается UI-контент экрана карты.
  */
 fun NavGraphBuilder.mapScreen() {
-    composable<MapRoute> {
+    composable<MapRoute> { navBackStackEntry ->
 
+        val context = LocalContext.current.applicationContext
+
+        val factory = (context as ComponentProvider)
+            .provideComponent("map", MapComponent::class)
+            .viewModelFactory
+
+        val viewModel = viewModel<MapViewModel>(
+            viewModelStoreOwner = navBackStackEntry,
+            factory = factory
+        )
+
+        val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+        MapScreen(uiState)
     }
 }
