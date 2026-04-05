2f23d1444b3d07f62f67438a33b72457406dfe54
Egor Bugay <egbugaj@gmail.com>
2026-04-05 16:09:25 +0600

    refactor: migrate to mapkit library

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,14 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+import java.util.Properties
+
+val mapkitApiKey: String = run {
+    val secretFile = file("secret.properties")
+    if (!secretFile.exists()) return@run ""
+    secretFile.inputStream().use { stream ->
+        Properties().apply { load(stream) }.getProperty("MAPKIT_API_KEY", "")
+    }
+}
+extra["mapkitApiKey"] = mapkitApiKey
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
