a9268acb53757544e9849ca48dcb2c68422128b8
Egor Bugay <egbugaj@gmail.com>
2026-08-04 01:06:14 +0600

    feature: create new memory screen module

--- feature/memory/build.gradle.kts
+++ feature/memory/build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.ksp)
 }
 
 android {
-    namespace = "ru.aloyaloya.here"
+    namespace = "ru.aloyaloya.memory"
     compileSdk {
         version = release(36) {
             minorApiLevel = 1
@@ -13,13 +14,10 @@ android {
     }
 
     defaultConfig {
-        applicationId = "ru.aloyaloya.here"
         minSdk = 28
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -42,14 +40,8 @@ android {
 
 dependencies {
     implementation(project(":core:ui"))
-    implementation(project(":core:mapkit"))
-    implementation(project(":core:design-system"))
-    implementation(project(":core:database"))
     implementation(project(":core:domain"))
-    implementation(project(":feature:map"))
-    implementation(project(":feature:memory"))
-    implementation(project(":feature:calendar"))
-    implementation(project(":feature:analytic"))
+    implementation(project(":core:design-system"))
 
     implementation(libs.dagger)
     ksp(libs.dagger.compiler)
@@ -57,6 +49,8 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
@@ -68,9 +62,4 @@ dependencies {
 
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
